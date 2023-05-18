2298ee27699cd338bfcf5b4922706b9229fe3dcb
stark5200 <a.taha5200@gmail.com>
2023-05-18 05:52:29 +0200

    added Banner

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 
-export const Nav = () => {
+export const NavBar = () => {
   const [ activeLink, setActiveLink ] = useState('home'); 
   const [ scrolled, setScrolled ] = useState(false);
 
