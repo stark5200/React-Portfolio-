59e459c2fd9509a394ea1f1130756ff630c0d267
stark5200 <a.taha5200@gmail.com>
2023-05-21 03:07:29 +0200

    done with Footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { Footer } from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Footer />
     </div>
   );
 }
